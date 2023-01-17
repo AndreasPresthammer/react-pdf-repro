a080e032d256f41eaedb760a325c7853d1d2699d
Andreas Presthammer <andhaug@gmail.com>
2023-01-17 21:14:28 +0100

    react-pdf twice render repro

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Document, Page, pdfjs} from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const url = "images/default/sample.pdf";
+    const [objectUrl, setObjectUrl] = useState<string>();
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(url);
+            const blob = await response.blob();
+            setObjectUrl(window.URL.createObjectURL(blob));
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            <a href={`https://www.africau.edu/${url}`}>Actual pdf</a>
+            {objectUrl && (
+                <Document file={objectUrl} onLoadError={console.error}>
+                    <Page pageNumber={1}/>
+                </Document>
+            )}
+        </div>
+    );
 }
 
 export default App;
